67a50d2595c0e4fbfaf344f2687d544c799ec433
Aqib Khan <aquibkhan8140@gmail.com>
2024-04-16 15:45:13 +0530

    new changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default;
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -34,12 +34,12 @@ app.use(express.static(path.join(__dirname,"/public")));
 // Database Connection
 
 
-
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
         secret: process.env.SECRET,
     },
+    ...options,
     touchAfter: 24 * 3600,
 });
 
